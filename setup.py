fbe7fec3b835ee8b6174388d7f6aa94de2870d4a
Alex de Landgraaf <alex@maykinmedia.nl>
2016-04-19 11:05:16 +0200

    Bumping versionnr as 0.0.2 broke PyPi (gave a HTTP 500 when uploading)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-regex-redirects',
-    version='0.0.2',
+    version='0.0.3',
     author=u'Alex de Landgraaf',
     author_email='alex@maykinmedia.nl',
     packages=find_packages(),
